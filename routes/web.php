e2580dab6f0a030af6dfa04de22c06b31d8a709a
rafael-santos-xrg <victorsantos.pba@outlook.com>
2023-05-28 22:57:13 -0300

    ADD: Apontando as views de cada rota Criando rotas com que retornam variaveis Criando rotas que possuem apelidos

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+Route::get('post/{id}/{name}', function ($id, $name) {
+    return ('Post número ===> ' . $id .  '</br>Postado por ===> ' . $name);
+});
+
+Route::get('this/is/a/really/very/long/url', array('as' => 'aliasURL', function () {
+    $url = route('aliasURL');
+    return ('Você acessou ' . $url);
+}));

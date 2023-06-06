1c5f804d1d05acaae1d4cff029f031e449268a29
rafael-santos-xrg <victorsantos.pba@outlook.com>
2023-06-05 22:36:24 -0300

    FIX: Controle de rotas pela função resource

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/about', function () {
+//     return view('about');
+// });
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Route::get('/contacts', function () {
+//     return view('contacts');
+// });
 
-Route::get('/contacts', function () {
-    return view('contacts');
-});
+// Route::get('post/{id}/{name}', function ($id, $name) {
+//     return ('Post número ===> ' . $id .  '</br>Postado por ===> ' . $name);
+// });
 
-Route::get('post/{id}/{name}', function ($id, $name) {
-    return ('Post número ===> ' . $id .  '</br>Postado por ===> ' . $name);
-});
+// Route::get('this/is/a/really/very/long/url', array('as' => 'aliasURL', function () {
+//     $url = route('aliasURL');
+//     return ('Você acessou ' . $url);
+// }));
 
-Route::get('this/is/a/really/very/long/url', array('as' => 'aliasURL', function () {
-    $url = route('aliasURL');
-    return ('Você acessou ' . $url);
-}));
+// Route::get('/post/{id}', [PostController::class, 'create']);
+
+Route::resource('/post', PostController::class);
